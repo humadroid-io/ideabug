6a0e5fc7f1746233b5cacbe5746b8dede19e23bf
Maciej Litwiniuk <maciej@litwiniuk.net>
2026-04-23 17:19:34 +0200

    When reading public key, fallback to BASE64 encoded version

--- config/initializers/jwt_config.rb
+++ config/initializers/jwt_config.rb
@@ -1,13 +1,23 @@
 require "openssl"
+require "base64"
 require "jwt"
 
 module JwtConfig
   class << self
     def public_key
-      @public_key ||= OpenSSL::PKey::RSA.new(
-        ENV["JWT_PUBLIC_KEY"] ||
-          File.read(Rails.root.join("config", "jwt", ENV.fetch("JWT_PUBLIC_KEY_FILE", "public.pem")))
-      )
+      @public_key ||= OpenSSL::PKey::RSA.new(raw_public_key)
+    end
+
+    private
+
+    def raw_public_key
+      if (b64 = ENV["JWT_PUBLIC_KEY_BASE64"].presence)
+        Base64.decode64(b64)
+      elsif (pem = ENV["JWT_PUBLIC_KEY"].presence)
+        pem
+      else
+        File.read(Rails.root.join("config", "jwt", ENV.fetch("JWT_PUBLIC_KEY_FILE", "public.pem")))
+      end
     end
   end
 end
